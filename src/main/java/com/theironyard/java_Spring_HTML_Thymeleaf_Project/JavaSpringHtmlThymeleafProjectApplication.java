331626f7dbd433c44f0164789ea82eefb9da08a4
Ian Lee <ian102423@github.com>
2017-08-01 13:28:03 -0500

    Initial Commit

---
+++ src/main/java/com/theironyard/java_Spring_HTML_Thymeleaf_Project/JavaSpringHtmlThymeleafProjectApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard.java_Spring_HTML_Thymeleaf_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringHtmlThymeleafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringHtmlThymeleafProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/theironyard/java_Spring_HTML_Thymeleaf_Project/JavaSpringHtmlThymeleafProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theironyard.java_Spring_HTML_Thymeleaf_Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaSpringHtmlThymeleafProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
